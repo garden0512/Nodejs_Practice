19ef7dbb73b160bf6d678f659328c99f2975fc46
정원 <88885421+garden0512@users.noreply.github.com>
2024-11-03 03:44:07 +0900

    백엔드

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express') //모듈 불러오기
+const path = require('path')
 var cors = require('cors') //모듈 불러오기
 const app = express()
-const port = 3000 //로컬호스트(컴)의 포트 번호
+const port = 5102 //로컬호스트(컴)의 포트 번호
 
 app.use(cors()) //cors 모듈을쓰겠다는 의미로, 여러 조건 설정이 가능함.
+app.use(express.static(path.join(__dirname, '../Web_front_practice')))
 
 //get은 http메소드, /은 라우팅(라우팅에 따라 보이는 페이지가 달라짐, youtube.com/channel/~~~~~~이런식으로 있는게 라우팅), (~~) =>{}는 콜백 함수
 app.get('/', (req, res) => { //req와 res에 정보를 담아 보낼 수 있음. req는 파라미터같은거 사용
-    res.send('Hello World!')
+    res.sendFile(path.join(__dirname + '/../Web_front_practice/main.html'))
 })
 
 app.get('/sound/:name', (req, res) => {
